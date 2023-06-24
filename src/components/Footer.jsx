49d551a80d45e4a8805dc258b5ef159def8fb39d
Neil <santoscneil@gmail.com>
2023-06-25 00:16:44 +0800

    added-charlz

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React from 'react';
 
 function Footer() {
   return (
     <footer className="border-t-6 bg-red-700 py-16 text-center">
-      <p className="text-white-500 text-2xl">
+      <p className="text-white-500 text-2xl ">
         TUP CAFA &copy; 2023 All rights reserved.
       </p>
     </footer>
